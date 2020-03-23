c19834a777c66bad6f1a379ffa03d97f2b9be068
李欢 <allnewhuanhuan@gmail.com>
2020-03-23 21:12:59 +0800

    18 commit

---
+++ iOS编程(第四版)/18-保存、读取与应用状态/Homepwner/Homepwner/HHAItemStore.m
@@ -0,0 +1,98 @@
+//
+//  HHAItemStore.m
+//  Homepwner
+//
+//  Created by 李欢 on 2020/3/8.
+//  Copyright © 2020 huan. All rights reserved.
+//
+
+#import "HHAItemStore.h"
+#import "HHAItem.h"
+#import "HHAImageStore.h"
+
+@interface HHAItemStore ()
+
+@property(nonatomic) NSMutableArray *privateItems;
+
+@end
+
+@implementation HHAItemStore
+
++ (instancetype)sharedStore {
+    static HHAItemStore *sharedStore = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedStore = [[self alloc] initPrivate];
+    });
+    return sharedStore;
+}
+
+- (instancetype)init
+{
+    @throw [NSException exceptionWithName:@"Singleton" reason:@"Use + [HHAItemStore sharedStore]" userInfo:nil];
+    return nil;
+}
+
+- (instancetype)initPrivate {
+    self = [super init];
+    if (self) {
+        NSString *path = [self itemArchivePath];
+        _privateItems = [NSKeyedUnarchiver unarchiveObjectWithFile:path];
+        
+        //如果之前没有保存过privateItems，就创建一个新的
+        if (!_privateItems) {
+            _privateItems = [[NSMutableArray alloc] init];
+        }
+    }
+    return self;
+}
+
+- (NSArray *)allItems {
+    return self.privateItems;
+}
+
+- (HHAItem *)createItem {
+    HHAItem *item = [[HHAItem alloc] init];
+    [self.privateItems addObject:item];
+    return item;
+}
+
+- (void)removeItem:(HHAItem *)item {
+    NSString *key = item.itemKey;
+    [[HHAImageStore sharedStore] deleteImageForKey:key];
+    [self.privateItems removeObjectIdenticalTo:item];
+}
+
+- (void)moveItemAtIndex:(NSUInteger)fromIndex
+                toIndex:(NSUInteger)toIndex {
+    if (fromIndex == toIndex) {
+        return;
+    }
+    //得到要移动对象的指针，以便稍后能将其插入新的位置
+    HHAItem *item = self.privateItems[fromIndex];
+    
+    //将item从allItems数组中移除
+    [self.privateItems removeObjectAtIndex:fromIndex];
+    
+    //根据新的索引位置，将item插回allItems数组
+    [self.privateItems insertObject:item atIndex:toIndex];
+}
+
+- (NSString *)itemArchivePath {
+    //注意第一个参数是NSDocumentDirectory而不是NSDocumentationDirectory
+    NSArray *documentDirectories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    
+    //从documentDirectories数组获取第一个，也是唯一文档目录路径
+    NSString *documentDirectory = [documentDirectories firstObject];
+    
+    return [documentDirectory stringByAppendingPathComponent:@"items.archive"];
+}
+
+- (BOOL)saveChanges {
+    NSString *path = [self itemArchivePath];
+    
+    //如果固化成功就返回YES
+    return [NSKeyedArchiver archiveRootObject:self.privateItems toFile:path];
+}
+
+@end
---
+++ iOS编程(第四版)/18-保存、读取与应用状态/Homepwner/Homepwner/HHAItemsViewController.m
@@ -0,0 +1,127 @@
+//
+//  HHAItemsViewController.m
+//  Homepwner
+//
+//  Created by 李欢 on 2020/3/8.
+//  Copyright © 2020 huan. All rights reserved.
+//
+
+#import "HHAItemsViewController.h"
+#import "HHADetailViewController.h"
+#import "HHAItemStore.h"
+#import "HHAItem.h"
+
+@interface HHAItemsViewController ()
+
+@property (nonatomic, strong) IBOutlet UIView *headerView;
+
+@end
+
+@implementation HHAItemsViewController
+
+- (instancetype)init
+{
+    self = [super initWithStyle:UITableViewStylePlain];
+    if (self) {
+        UINavigationItem *navItem = self.navigationItem;
+        navItem.title = @"Homepwner";
+
+        //创建新的UIBarButtonItem对象
+        //将其目标对象设置为当前对象，将其动作方法设置为addNewItem：
+        UIBarButtonItem *bbi = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewItem:)];
+
+        //为UINavigationItem对象的rightBarButtonItem属性赋值，
+        //指向新创建的UIBarButtonItem对象
+        navItem.rightBarButtonItem = bbi;
+        navItem.leftBarButtonItem = self.editButtonItem;
+    }
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style {
+    return [self init];
+}
+
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+//    HHADetailViewController *detailViewController = [[HHADetailViewController alloc] init];
+    HHADetailViewController *detailViewController = [[HHADetailViewController alloc] initForNewItem:NO];
+    NSArray *items = [[HHAItemStore sharedStore] allItems];
+    HHAItem *selectedItem = items[indexPath.row];
+
+    //将选中的HHAItem对象赋给DetailViewController对象
+    detailViewController.item = selectedItem;
+    //将新创建的HHADetailViewController对象压入UINavigationController对象的栈
+    [self.navigationController pushViewController:detailViewController animated:YES];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [[HHAItemStore sharedStore] allItems].count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+
+    NSArray *items = [[HHAItemStore sharedStore] allItems];
+    HHAItem *item = items[indexPath.row];
+
+    cell.textLabel.text = [item description];
+
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    //如果UITableView对象请求确认的是删除操作......
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSArray *items = [[HHAItemStore sharedStore] allItems];
+        HHAItem *item = items[indexPath.row];
+        [[HHAItemStore sharedStore] removeItem:item];
+
+        //还要删除表格视图中的相应表格行（带动画效果）
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
+}
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
+    [[HHAItemStore sharedStore] moveItemAtIndex:sourceIndexPath.row toIndex:destinationIndexPath.row];
+}
+
+- (void)viewDidLoad {
+    NSLog(@"viewDidLoad");
+    [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+    UIView *header = self.headerView;
+    [self.tableView setTableHeaderView:header];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    NSLog(@"viewWillAppear");
+    [super viewWillAppear:animated];
+    [self.tableView reloadData];
+}
+
+- (IBAction)addNewItem:(id)sender {
+    //创建新的HHAItem对象并将其加入HHAItemStore
+    HHAItem *newItem = [[HHAItemStore sharedStore] createItem];
+
+    HHADetailViewController *detailViewController = [[HHADetailViewController alloc] initForNewItem:YES];
+
+    detailViewController.item = newItem;
+
+    detailViewController.dismissBlock = ^{
+//        NSInteger lastRow = [[[HHAItemStore sharedStore] allItems] indexOfObject:newItem];
+//        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:lastRow inSection:0];
+//
+//        [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
+        [self.tableView reloadData];
+    };
+
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:detailViewController];
+
+    navController.modalPresentationStyle = UIModalPresentationFormSheet;
+
+    [self presentViewController:navController animated:YES completion:nil];
+}
+
+@end
